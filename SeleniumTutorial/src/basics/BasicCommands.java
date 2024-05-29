75ffd30a84192bfb541d74537ab53885101ff387
Sys <Sys@DESKTOP-4GAOFMD>
2024-05-29 14:51:24 +0530

    Commit

---
+++ SeleniumTutorial/src/basics/BasicCommands.java
@@ -0,0 +1,38 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicCommands {
+
+	public static void main(String[] args) {
+		// Ctrl+shift+o (to import webdriver and chromedriver)
+        WebDriver driver = new ChromeDriver();
+          
+		//will maximize chrome
+        driver.manage().window().maximize();
+		
+		//will open a URL
+		driver.get("https://www.flipkart.com/");
+		
+		//to grabTitle of page
+		String title=driver.getTitle();
+		System.out.println(title);
+			
+		//to grab current URL
+		System.out.println(driver.getCurrentUrl());
+		
+		//to navigate in back direction		
+		driver.navigate().back();
+				
+		//to navigate in forward direction
+		driver.navigate().forward();
+				
+		//will close current window
+		driver.close();
+		
+		//close entire browser
+		driver.quit();
+		
+	}
+}
